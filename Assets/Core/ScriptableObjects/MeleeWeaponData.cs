0cf0b086423ea13590792d0bd7204c8c4f642edc
Xein <xgonip@gmail.com>
2024-10-13 19:06:01 +0200

    Revised weapons with bug fixes

--- Assets/Core/ScriptableObjects/MeleeWeaponData.cs
+++ Assets/Core/ScriptableObjects/MeleeWeaponData.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "WD_UnnamedMeleeWeaponData", menuName = "ProjectHotline/Create MeleeWeaponData")]
+[CreateAssetMenu(fileName = "WD_UnnamedMelee", menuName = "ProjectHotline/Create MeleeWeaponData")]
 public class MeleeWeaponData : ScriptableObject
 {
     public float range = .5f;
