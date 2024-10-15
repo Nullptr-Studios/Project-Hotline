529d8291a040efae5d81b5ada46a87e01450271b
Missing Textures <dariormt@gmail.com>
2024-10-15 16:23:22 +0200

    Added Performance manager

---
+++ Assets/VFX/DynamicBlood/FloorSplatterDissapear.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class FloorSplatterDisapear : MonoBehaviour
+{
+
+    public float lerpMagnitudeMax = 2;
+    public float lerpMagnitudeMin = 1;
+
+    private float finalLerpMagnitude;
+
+    private void Start()
+    {
+        finalLerpMagnitude = UnityEngine.Random.Range(lerpMagnitudeMin, lerpMagnitudeMax);
+    }
+
+    void Update()
+    {
+        if (transform.localScale.x > .05f)
+        {
+            transform.localScale = new Vector3(math.lerp(transform.localScale.x, 0, finalLerpMagnitude * Time.deltaTime),
+                math.lerp(transform.localScale.y, 0, finalLerpMagnitude * Time.deltaTime), 1);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
