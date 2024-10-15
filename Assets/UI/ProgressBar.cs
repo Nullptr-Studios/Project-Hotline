ef059940d70984f8d79e671da6e77ddb6d1eb11d
Xein <xgonip@gmail.com>
2024-10-15 10:51:41 +0300

    Progress Bar done
    Implemented more things to the progressBar, making it easier to use. Also fixed a bug with dialog rescaling.

--- Assets/UI/ProgressBar.cs
+++ Assets/UI/ProgressBar.cs
@@ -1,29 +1,117 @@
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ProgressBar : MonoBehaviour
 {
-    private TextMeshProUGUI _textUI;
+    [SerializeField] private float duration;
+
+    [Header("Components")]
+    [SerializeField] private TextMeshProUGUI textUI;
+    [SerializeField] private Image background;
+    [SerializeField] private Image divider;
+
+    private bool _active;
+    private bool _isInverse;
     private float _value;
+    private float _current;
+    private float _dividerValue;
+    private int _size;
     private readonly List<char> _bar = new List<char>();
 
     /// <summary>
     /// Returns current percentage
     /// </summary>
     public float Value => _value;
+
+    /// <summary>
+    /// Returns if the current progress has surpassed the divider value
+    /// </summary>
+    public bool AboveDivider => _current > _dividerValue;
     
     private void Awake()
     {
-        _textUI = GetComponentInChildren<TextMeshProUGUI>();
+        textUI = GetComponentInChildren<TextMeshProUGUI>();
         SetValue(0);
+        Hide();
+    }
+
+    private void Update()
+    {
+        if (_active)
+        {
+            if (!_isInverse)
+            {
+                _current += Time.deltaTime / duration;
+                SetValue(_current);
+
+                if (_current >= 1)
+                {
+                    SetValue(0);
+                    Hide();
+                    _active = false;
+                }
+            }
+            else 
+            {
+                _current += Time.deltaTime / duration;
+                SetValue(1 - _current);
+
+                if (1 - _current <= 0)
+                {
+                    SetValue(0);
+                    Hide();
+                    _active = false;
+                }
+            }
+
+            // Color check for under divider value
+            if (_current < _dividerValue)
+                textUI.color = Color.red;
+            else
+                textUI.color = Color.white;
+                
+        }
+    }
+
+    /// <summary>
+    /// Initializes the progress bar timer
+    /// </summary>
+    /// <param name="time">Time to complete</param>
+    /// <param name="isInverse">Go from <c>1f</c> to <c>0f</c> </param>
+    /// <param name="size">Number of characters in the progress bar</param>
+    /// <param name="hasDivider">Tells if bar has a division</param>
+    /// <param name="dividerSize">Percentage of the bar division</param>
+    public void BeginTimer(float time, bool isInverse = false, int size = 10, bool hasDivider = false, float dividerSize = 0f)
+    {
+        duration = time;
+        _isInverse = isInverse;
+        _dividerValue = dividerSize;
+        _size = size;
+
+        background.rectTransform.sizeDelta = new Vector2(size * 4, background.rectTransform.sizeDelta.y);
+
+        if (!hasDivider)
+        {
+            divider.enabled = false;
+        }
+        else
+        {
+            if (dividerSize <= 0) Debug.LogWarning("[ProgressBar] Slider divider is on but Size is zero");
+            divider.rectTransform.sizeDelta = new Vector2(_dividerValue * size * 4f, divider.rectTransform.sizeDelta.y);
+        }
+
+        Show();
+        SetValue(isInverse? 1 : 0);
+        _active = true;
     }
 
     /// <summary>
     /// Set progress bar to given percentage and updates the UI
     /// </summary>
     /// <param name="value">Percentage from <c>0f</c> to <c>1f</c></param>
-    public void SetValue(float value)
+    private void SetValue(float value)
     {
         _value = Mathf.Clamp01(value);
 
@@ -34,32 +122,32 @@ public class ProgressBar : MonoBehaviour
         // I know I can probably do this more cleanly but fuck this progress bar -x
         if (_value >= 1)
         {
-            for (var i = 0; i < 10; i++) _bar.Add('\u2588');
-            _textUI.text = new string(_bar.ToArray());
+            for (var i = 0; i < _size; i++) _bar.Add('\u2588');
+            textUI.text = new string(_bar.ToArray());
             return;
         }
 
         while (currentProgress < _value)
         {
-            if (currentProgress + 10/100f <= _value)
+            if (currentProgress + 1f/_size <= _value)
             {
                 _bar.Add('\u2588');
-                currentProgress += 1/10f;
+                currentProgress += 1f/_size;
             }
-            else if (currentProgress + 7.5/100f <= _value)
+            else if (currentProgress + 0.75f/_size <= _value)
             {
                 _bar.Add('\u2593');
-                currentProgress += 75/1000f;
+                currentProgress += 0.75f/_size;
             }
-            else if (currentProgress + 5/100f <= _value)
+            else if (currentProgress + 0.5f/_size <= _value)
             {
                 _bar.Add('\u2592');
-                currentProgress += 5 / 100f;
+                currentProgress += 0.5f/_size;
             }
-            else if (currentProgress + 2.5/100f <= _value)
+            else if (currentProgress + 0.25f/_size <= _value)
             {
                 _bar.Add('\u2591');
-                currentProgress += 25/1000f;
+                currentProgress += 0.25f/_size;
             }
             else 
             {
@@ -67,6 +155,20 @@ public class ProgressBar : MonoBehaviour
             }
         } 
         
-        _textUI.text = new string(_bar.ToArray());
+        textUI.text = new string(_bar.ToArray());
+    }
+
+    private void Show()
+    {
+        textUI.enabled = true;
+        background.enabled = true;
+        divider.enabled = true;
+    }
+
+    private void Hide()
+    {
+        textUI.enabled = false;
+        background.enabled = false;
+        divider.enabled = false;
     }
 }
