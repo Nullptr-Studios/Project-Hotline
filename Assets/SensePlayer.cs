61185663a76f9852e9f4e67b528ec4663cf16367
Missing Textures <dariormt@gmail.com>
2024-10-18 21:51:55 +0200

    UNSTABLE first version of state machine

---
+++ Assets/SensePlayer.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TheKiwiCoder;
+
+public class SensePlayer : ActionNode
+{
+    private AISensor sensor;
+    
+    private bool hasSensedPlayerBefore = false;
+    protected override void OnStart() 
+    {
+        sensor = context.gameObject.GetComponent<AISensor>();
+        hasSensedPlayerBefore = false;
+
+    }
+
+    protected override void OnStop() {
+    }
+
+    protected override State OnUpdate() {
+        if (sensor.isDetecting)
+        {
+            blackboard.playerPos = sensor.detectedObjects[0].transform.position;
+            context.agent.SetDestination(blackboard.playerPos);
+            hasSensedPlayerBefore = true;
+        }else if (hasSensedPlayerBefore)
+        {
+            if(context.agent.remainingDistance <= context.agent.stoppingDistance)
+                return State.Success;
+        }
+        return State.Running;
+    }
+}
