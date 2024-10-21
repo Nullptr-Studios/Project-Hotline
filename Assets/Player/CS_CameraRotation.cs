dd82f4c3a8624e7d29962ee55da45162481b1d5d
Xein <xgonip@gmail.com>
2024-10-21 14:06:56 +0300

    Created camera rotation

---
+++ Assets/Player/CS_CameraRotation.cs
@@ -0,0 +1,54 @@
+using Cinemachine;
+using UnityEngine;
+
+public class CameraRotation : MonoBehaviour
+{
+    [SerializeField] private float maxRotation;
+    [SerializeField] private float maxDistance;
+    private float baseRotation;
+
+    [Header("Components")]
+    [SerializeField] private GameObject player;
+    [SerializeField] private CinemachineVirtualCamera camera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        baseRotation = camera.transform.eulerAngles.z;
+
+        if (player == null || camera == null)
+        {
+            Debug.LogWarning($"[Camera Rotation] {this.name}: No reference for player or camera. Disabling camera rotation.");
+            gameObject.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var distance = player.transform.position.x - transform.position.x;
+        var rotation = maxRotation * Mathf.Clamp((distance / maxDistance), -1, 1) + baseRotation;
+        camera.transform.eulerAngles = new Vector3(0f, 0f, rotation);
+    }
+
+#if UNITY_EDITOR
+
+    [Header("Debug")]
+    [SerializeField] private float lineLength;
+
+    private void OnDrawGizmos()
+    {
+        // World divider gizmo
+        var startPos = new Vector3(transform.position.x, transform.position.y + lineLength/2, transform.position.z);
+        var endPos = new Vector3(transform.position.x, transform.position.y - lineLength/2, transform.position.z);
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(startPos, endPos);
+
+        // Max rotation gizmo
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(transform.position, new Vector3(maxDistance*2, 1, 1));
+    }
+
+#endif
+
+}
