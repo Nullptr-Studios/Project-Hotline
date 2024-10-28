c3de149788050b028953ea45a0162f7872a3c568
Omagoge <inakiomagoge@gmail.com>
2024-10-28 13:59:11 +0200

    Save system begining

---
+++ Assets/SaveSystem.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using ToolBox.Serialization;
+using UnityEditor;
+using System.Collections.Generic;
+
+public class SaveSystem : MonoBehaviour
+{
+ 
+}
+
+public class SaveKeywords
+{
+    public const string LevelScore = "LevelScore";
+    public const string TestValue = "TestValue";
+}
+
+#if UNITY_EDITOR
+[CustomEditor(typeof(SaveSystem))]
+public class SaveSystemEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        var saveDataUtilities = (SaveSystem)target;
+        if (saveDataUtilities == null) return;
+
+        Undo.RecordObject(saveDataUtilities, "SaveDataUtilities");
+
+        if (GUILayout.Button("Delete All Saved Data"))
+        {
+            DataSerializer.DeleteAll();
+        }
+    }
+}
+#endif
