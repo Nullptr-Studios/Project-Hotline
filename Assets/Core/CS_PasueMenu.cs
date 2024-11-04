63d63ff23d18f14d63b4fb2c6e518a322ee3262f
Xein <xgonip@gmail.com>
2024-11-04 14:41:18 +0200

    Continued PauseMenu

--- Assets/Core/CS_PasueMenu.cs
+++ Assets/Core/CS_PasueMenu.cs
@@ -3,24 +3,36 @@ using UnityEngine;
 public class PasueMenu : MonoBehaviour
 {
     private Canvas _canvas;
+    private PlayerMovement _input;
+    private Animator _animator;
 
     private void Awake()
     {
         _canvas = GetComponent<Canvas>();
+        _animator = GetComponentInChildren<Animator>();
+        _input = GameObject.FindWithTag("Player").GetComponent<PlayerMovement>();
     }
 
-    private void OnFinishAnimation()
+    /*private void OnFinishAnimation()
     {
         Time.timeScale = 0f;
-    }
+    }*/
 
     private void OnDisable()
     {
         Time.timeScale = 1f;
     }
 
+    private void OnEnable()
+    {
+        _input.OnDisable();
+    }
+
     public void OnContinue()
-    { }
+    { 
+        _input.OnEnable();
+        _animator.SetTrigger("CloseAnim");
+    }
 
     public void OnExit()
     {
