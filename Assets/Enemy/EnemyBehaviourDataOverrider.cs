76c3f353863b1864462ec2f90457efdb1855b7ae
Missing Textures <dariormt@gmail.com>
2024-11-09 13:22:27 +0100

    base enemy sprites changed

--- Assets/Enemy/EnemyBehaviourDataOverrider.cs
+++ Assets/Enemy/EnemyBehaviourDataOverrider.cs
@@ -2,12 +2,46 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyBehaviourDataOverrider : MonoBehaviour
 {
     public EnemyBehaviourData behaviourData;
 
     public bool justStunned = false;
+
+    public Animator animatorEnemy;
+    public Animator animatorEnemyFoot;
+    public Transform footTransform;
+
+    private NavMeshAgent _rb;
+    
+    private static readonly int AnimRate = Animator.StringToHash("AnimRate");
+    private static readonly int IsIdle = Animator.StringToHash("IsIdle");
+
+    private void Start()
+    {
+        _rb = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        // Animation stuff
+
+        if (_rb.velocity.magnitude > 0.1f)
+        {
+            float angle = Mathf.Atan2(_rb.velocity.y, _rb.velocity.x) * Mathf.Rad2Deg;
+            footTransform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+            animatorEnemyFoot.enabled = true;
+            animatorEnemy.SetBool(IsIdle, false);
+        }
+        else
+        {
+            animatorEnemyFoot.enabled = false;
+            animatorEnemy.SetBool(IsIdle, true);
+        }
+    }
 
 #if UNITY_EDITOR
     [Header("Debug")] 
