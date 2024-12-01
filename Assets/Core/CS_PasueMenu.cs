a9bfff897ca042e0d0862a0db1468805b0a60643
Missing Textures <dariormt@gmail.com>
2024-12-02 00:06:55 +0100

    Fixed shit

--- Assets/Core/CS_PasueMenu.cs
+++ Assets/Core/CS_PasueMenu.cs
@@ -9,6 +9,9 @@ public class PauseMenu : MonoBehaviour
     private PlayerIA _input;
     private Animator _animator;
 
+    public delegate void Pause(bool pause);
+    public static Pause OnPause;
+    
     private void Awake()
     {
         _animator = GetComponentInChildren<Animator>();
@@ -27,6 +30,7 @@ public class PauseMenu : MonoBehaviour
     private void OnEnable()
     {
         _playerinput.OnDisable();
+        OnPause?.Invoke(true);
     }
 
     // This happens when the open animations finish
@@ -44,6 +48,7 @@ public class PauseMenu : MonoBehaviour
         _playerinput.OnEnable();
         _input.UI.Cancel.Disable();
         _animator.SetTrigger(CloseAnim);
+        OnPause?.Invoke(false);
     }
 
     // Exit to main menu button
--- Assets/Core/Managers/MusicManager.cs
+++ Assets/Core/Managers/MusicManager.cs
@@ -1,45 +1,151 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using FMOD.Studio;
 using FMODUnity;
 using UnityEngine;
+using UnityEngine.Serialization;
 using STOP_MODE = FMOD.Studio.STOP_MODE;
 
 public class MusicManager : MonoBehaviour
 {
     
-    //@TODO: Implement a way to change the music dynamically, for now this works
+    class TimelineInfo
+    {
+        public int currentMusicBar = 0;
+        public int currentMusicBeat = 0;
+        public FMOD.StringWrapper lastMarker = new FMOD.StringWrapper();
+    }
+
+    TimelineInfo timelineInfo;
+    GCHandle timelineHandle;
+    
     public EventReference sceneMusic;
     
     private FMOD.Studio.EventInstance _gameMusicInstance;
+    
+    private FMOD.Studio.EVENT_CALLBACK beatCallback;
+    
+    public bool hasDialogMusic = false;
+    
+    public int lastbar = 0;
+    public int lastBeat;
+
+    public delegate void Beat();
+    public delegate void Bar();
+    public static Bar OnBar;
+    public static Beat OnBeat;
 
     
     // Start is called before the first frame update
     void Start()
     {
+        timelineInfo = new TimelineInfo();
+        
+        // Explicitly create the delegate object and assign it to a member so it doesn't get freed
+        // by the garbage collected while it's being used
+        beatCallback = new FMOD.Studio.EVENT_CALLBACK(BeatEventCallback);
+
+        
         _gameMusicInstance = FMODUnity.RuntimeManager.CreateInstance(sceneMusic);
         
+        // Pin the class that will store the data modified during the callback
+        timelineHandle = GCHandle.Alloc(timelineInfo);
+        // Pass the object through the userdata of the instance
+        _gameMusicInstance.setUserData(GCHandle.ToIntPtr(timelineHandle));
+
+        _gameMusicInstance.setCallback(beatCallback, FMOD.Studio.EVENT_CALLBACK_TYPE.TIMELINE_BEAT);
+        
         NovelUIController.OnStartGame += Startscene;
+        LoadingScreen.OnFinalizedLoading += endedLoading;
+        _gameMusicInstance.setParameterByName("DialogueLoop", 1);
+        _gameMusicInstance.setParameterByName("LowPass",0);
+        
+        PauseMenu.OnPause += Pause;
     }
 
+    private void Pause(bool pause)
+    {
+        if(pause)
+            _gameMusicInstance.setParameterByName("LowPass",1);
+        else
+            _gameMusicInstance.setParameterByName("LowPass",0);
+    }
+
+    private void endedLoading()
+    {
+        if(hasDialogMusic)
+            _gameMusicInstance.start();
+    }
     private void OnDestroy()
     {
         NovelUIController.OnStartGame -= Startscene;
+        _gameMusicInstance.setUserData(IntPtr.Zero);
+        _gameMusicInstance.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+        _gameMusicInstance.release();
+        timelineHandle.Free();
     }
 
     private void Startscene()
     {
-        _gameMusicInstance.start();
+        //_gameMusicInstance.start();
+        _gameMusicInstance.setParameterByName("DialogueLoop", 0);
+        if(!hasDialogMusic)
+            _gameMusicInstance.start();
     }
-
-    private void OnDisable()
+    
+    [AOT.MonoPInvokeCallback(typeof(FMOD.Studio.EVENT_CALLBACK))]
+    static FMOD.RESULT BeatEventCallback(FMOD.Studio.EVENT_CALLBACK_TYPE type, IntPtr instancePtr, IntPtr parameterPtr)
     {
-        _gameMusicInstance.stop(STOP_MODE.ALLOWFADEOUT);
+        FMOD.Studio.EventInstance instance = new FMOD.Studio.EventInstance(instancePtr);
+
+        // Retrieve the user data
+        IntPtr timelineInfoPtr;
+        FMOD.RESULT result = instance.getUserData(out timelineInfoPtr);
+        if (result != FMOD.RESULT.OK)
+        {
+            Debug.LogError("Timeline Callback error: " + result);
+        }
+        else if (timelineInfoPtr != IntPtr.Zero)
+        {
+            // Get the object to store beat and marker details
+            GCHandle timelineHandle = GCHandle.FromIntPtr(timelineInfoPtr);
+            TimelineInfo timelineInfo = (TimelineInfo)timelineHandle.Target;
+
+            switch (type)
+            {
+                case FMOD.Studio.EVENT_CALLBACK_TYPE.TIMELINE_BEAT:
+                {
+                    var parameter = (FMOD.Studio.TIMELINE_BEAT_PROPERTIES)Marshal.PtrToStructure(parameterPtr, typeof(FMOD.Studio.TIMELINE_BEAT_PROPERTIES));
+                    timelineInfo.currentMusicBar = parameter.bar;
+                    timelineInfo.currentMusicBeat = parameter.beat;
+                }
+                    break;
+            }
+        }
+        return FMOD.RESULT.OK;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        if(lastbar != timelineInfo.currentMusicBar)
+        {
+            lastbar = timelineInfo.currentMusicBar;
+            Debug.Log("Bar!");
+            OnBar?.Invoke();
+            return;
+        }
+        
+        if(lastBeat != timelineInfo.currentMusicBeat)
+        {
+            lastBeat = timelineInfo.currentMusicBeat;
+            lastbar = timelineInfo.currentMusicBar;
+            Debug.Log("Beat!");
+            OnBeat?.Invoke();
+            return;
+        }
     }
 }
